a66240b624157bd08cf76400b333c90658177cfc
jrimet <johan.rimet@preciamolen.fr>
2019-07-23 13:33:44 +0200

    test

--- action-date.py
+++ action-date.py
@@ -1,22 +1,56 @@
 #!/usr/bin/env python3
 from hermes_python.hermes import Hermes
-from datetime import date
+from datetime import datetime
 from pytz import timezone
 
 MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
+
+def verbalise_hour(i):
+	if i == 0:
+		return "minuit"
+	elif i == 1:
+		return "une heure"
+	elif i == 12:
+		return "midi"
+	elif i == 21:
+		return "vingt et une heures"
+	else:
+		return "{0} heures".format(str(i)) 
+
+def verbalise_minute(i):
+	if i == 0:
+		return ""
+	elif i == 1:
+		return "une"
+	elif i == 21:
+		return "vingt et une"
+	elif i == 31:
+		return "trente et une"
+	elif i == 41:
+		return "quarante et une"
+	elif i == 51:
+		return "cinquante et une"
+	else:
+		return "{0}".format(str(i)) 
+
+
 def intent_received(hermes, intent_message):
 	print(intent_message.intent.intent_name)
 
-	MonthList = ['Janvier','Fevrier','Mars','Avril','Mai','Juin','Juillet','Aout','Septembre','Octobre','Novembre','Decembre']
-	DayList = ['Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi','Dimanche']
-	dayNumber = date.today().day
-	weekday = DayList[date.today().weekday()-1]
-	sentence = "On est le " + weekday + " " + str(dayNumber) + " "+ MonthList[date.today().month-1]
-	print(sentence)
-	hermes.publish_end_session(intentMessage.session_id, sentence)
+	if intent_message.intent.intent_name == 'ustaN:date':
+
+		sentence = 'Il est '
+		print(intent_message.intent.intent_name)
+
+		now = datetime.now(timezone('Europe/Paris'))
+
+		sentence += verbalise_hour(now.hour) + " " + verbalise_minute(now.minute)
+		print(sentence)
+
+		hermes.publish_end_session(intent_message.session_id, sentence)
 
 with Hermes(MQTT_ADDR) as h:
 	h.subscribe_intents(intent_received).start()

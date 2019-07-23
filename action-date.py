0e341beef5a88b627279ae82b330d4560892dacf
jrimet <johan.rimet@preciamolen.fr>
2019-07-23 11:25:51 +0200

    3

--- action-date.py
+++ action-date.py
@@ -10,14 +10,13 @@ MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 def intent_received(hermes, intent_message):
 	print(intent_message.intent.intent_name)
 
-	if intentMessage.intent.intent_name == 'ustaN:date':
-		MonthList = ['Janvier','Fevrier','Mars','Avril','Mai','Juin','Juillet','Aout','Septembre','Octobre','Novembre','Decembre']
-		DayList = ['Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi','Dimanche']
-		dayNumber = date.today().day
-		weekday = DayList[date.today().weekday()-1]
-		sentence = "On est le " + weekday + " " + str(dayNumber) + " "+ MonthList[date.today().month-1]
-		print(sentence)
-		hermes.publish_end_session(intentMessage.session_id, sentence)
+	MonthList = ['Janvier','Fevrier','Mars','Avril','Mai','Juin','Juillet','Aout','Septembre','Octobre','Novembre','Decembre']
+	DayList = ['Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi','Dimanche']
+	dayNumber = date.today().day
+	weekday = DayList[date.today().weekday()-1]
+	sentence = "On est le " + weekday + " " + str(dayNumber) + " "+ MonthList[date.today().month-1]
+	print(sentence)
+	hermes.publish_end_session(intentMessage.session_id, sentence)
 
 with Hermes(MQTT_ADDR) as h:
 	h.subscribe_intents(intent_received).start()
